0d364fd73558cd8fb4013258f4839939480fa381
Michael Gottesman <mgottesman@apple.com>
2016-11-24 23:25:30 -0600

    auto

---
+++ utils/metaprogram/metaprogram/metaprogram.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#===--- metaprogram.py ---------------------------------------------------===#
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See http:#swift.org/LICENSE.txt for license information
+# See http:#swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+#
+#===----------------------------------------------------------------------===#
+#
+# This is a simple python program that parses json files and builds up a generic 
+#
+#===----------------------------------------------------------------------===#
+
+import os
+import sys
+import json
+
+class Category(object):
+    def __init__(self):
+        pass
+
+class Entry(object):
+    def __init__(self):
+        pass
+
+class Data(object):
+    def __init__(self):
+        pass

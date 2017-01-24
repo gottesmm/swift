1250a610a29d91d4295c7be90a04d8e2c2e6e867
Michael Gottesman <mgottesman@apple.com>
2017-01-23 16:27:40 -0800

    [semantic-sil] When emitting a direct argument for a callsite, if the value is not guaranteed after we call getAsSingleValue(), borrow the value.
    The reason why we do not want to do this in getAsSingleValue itself is that
    getAsSingleValue is asking the question of, can the argument source provide the
    object at +0. We are handling the case of whether or not the argument source
    providing the object at +0, we need it at +0, so create a begin_borrow context.

--- lib/SILGen/SILGenApply.cpp
+++ lib/SILGen/SILGenApply.cpp
@@ -3151,6 +3151,13 @@ namespace {
         }
       } else {
         value = std::move(arg).getAsSingleValue(SGF, contexts.ForEmission);
+
+        // If our emitted value is not guaranteed, but we need a guaranteed
+        // parameter, insert the borrow.
+        if (param.getConvention() == ParameterConvention::Direct_Guaranteed &&
+            value.getOwnershipKind() != ValueOwnershipKind::Guaranteed) {
+          value = value.borrow(SGF, arg.getLocation());
+        }
       }
       Args.push_back(value);
     }

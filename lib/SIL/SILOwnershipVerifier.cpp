532c4cf65ee1ba4cf90df6c8763fc7baeb1d5e81
Michael Gottesman <mgottesman@apple.com>
2017-01-30 12:21:41 -0800

    [ownership-verifier] Another place I forgot to change getType().isTrivial(Mod) to a check against ValueOwnershipKind::Trivial due to enums.

--- lib/SIL/SILOwnershipVerifier.cpp
+++ lib/SIL/SILOwnershipVerifier.cpp
@@ -207,7 +207,7 @@ public:
   bool isAddressOrTrivialType() const {
     if (getType().isAddress())
       return true;
-    return getType().isTrivial(Mod);
+    return getOwnershipKind() == ValueOwnershipKind::Trivial;
   }
 
   OwnershipUseCheckerResult visitForwardingInst(SILInstruction *I);

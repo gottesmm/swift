1b355baa679ed88690dafe2410c0018a77c31615
Michael Gottesman <mgottesman@apple.com>
2023-05-24 12:51:47 -0700

    auto

--- lib/SILOptimizer/Mandatory/MoveOnlyCanonicalizer.cpp
+++ lib/SILOptimizer/Mandatory/MoveOnlyCanonicalizer.cpp
@@ -32,51 +32,6 @@ namespace {
 
 class MoveOnlyCanonicalizer : public SILFunctionTransform {
   void run() override {
-    auto *fn = getFunction();
-
-    // Only run this pass if the move only language feature is enabled.
-    if (!fn->getASTContext().supportsMoveOnlyTypes())
-      return;
-
-    // Don't rerun diagnostics on deserialized functions.
-    if (getFunction()->wasDeserializedCanonical())
-      return;
-
-    assert(fn->getModule().getStage() == SILStage::Raw &&
-           "Should only run on Raw SIL");
-
-    // If an earlier pass told use to not emit diagnostics for this function,
-    // clean up any copies, invalidate the analysis, and return early.
-    if (getFunction()->hasSemanticsAttr(semantics::NO_MOVEONLY_DIAGNOSTICS)) {
-      if (cleanupNonCopyableCopiesAfterEmittingDiagnostic(getFunction()))
-        invalidateAnalysis(SILAnalysis::InvalidationKind::Instructions);
-      return;
-    }
-
-    LLVM_DEBUG(llvm::dbgs()
-               << "===> MoveOnly Checker. Visiting: " << fn->getName() << '\n');
-
-    MoveOnlyChecker checker(
-        getFunction(), getAnalysis<DominanceAnalysis>()->get(getFunction()),
-        getAnalysis<PostOrderAnalysis>());
-
-    checker.checkObjects();
-    checker.checkAddresses();
-
-    // If we did not emit any diagnostics, emit an error on any copies that
-    // remain. If we emitted a diagnostic, we just want to rewrite all of the
-    // non-copyable copies into explicit variants below and let the user
-    // recompile.
-    if (!checker.diagnosticEmitter.emittedDiagnostic()) {
-      emitCheckerMissedCopyOfNonCopyableTypeErrors(getFunction(),
-                                                   checker.diagnosticEmitter);
-    }
-
-    checker.madeChange |=
-        cleanupNonCopyableCopiesAfterEmittingDiagnostic(getFunction());
-
-    if (checker.madeChange)
-      invalidateAnalysis(SILAnalysis::InvalidationKind::Instructions);
   }
 };
 
